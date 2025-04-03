f62fffc706b6b5f88ab0ddcbfd91d407072e85b9
caojiyuan <996790789@qq.com>
2025-04-03 11:14:48 +0800

    3

--- DeepSeek.py
+++ DeepSeek.py
@@ -1,13 +1,13 @@
 
 from openai import OpenAI
 
-client = OpenAI(api_key="<DeepSeek API Key>", base_url="https://api.deepseek.com")
+client = OpenAI(api_key="sk-6f8a3dd17777483cac209df838c04aa9", base_url="https://api.deepseek.com")
 
 response = client.chat.completions.create(
     model="deepseek-chat",
     messages=[
         {"role": "system", "content": "You are a helpful assistant"},
-        {"role": "user", "content": "Hello"},
+        {"role": "user", "content": "作一首诗，描写美女"},
     ],
     stream=False
 )
